290c9b14e3dd90809093df4624d978c517b032a7
Monis9451 <mianmonishussain786@gmail.com>
2025-01-26 20:09:38 +0500

    practicing tuple

---
+++ Week 2/Manual3.py
@@ -0,0 +1,6 @@
+cars = ["mustang", "audi", "toyota", "bmw", "mercedes"]
+cars.append("honda")
+print(cars)
+for i in range(0,len(cars)):
+    print(cars[i])
+print(len(cars))
---
+++ Week 2/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+cars = ["mustang", "audi", "toyota", "bmw", "mercedes"];
+print(cars)
+for i in range(0,len(cars)):
+    print(cars[i])
